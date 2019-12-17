807d55d1d9ca9ca367a64f8077e2606650d3fdb3
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-17 20:14:32 +0530

    Layout component and burgerbuilder container

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
+
+const layout = (props) =>(
+    <Aux>
+    <div>Toolbar, Sidebar, Backdrop</div>
+    <main className={classes.Content}>
+        {props.children}
+    </main>
+    </Aux>
+)
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Auxiliary';
+
+export class BurgerBuilder extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build controls</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
