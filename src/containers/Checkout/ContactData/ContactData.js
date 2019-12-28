5a364532a4f34987a548140dd785ada8ea27007a
abhinav7693ngh <abhinav7693ngh@gmail.com>
2019-12-28 19:44:21 +0530

    Custom Input Element in UI Components

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -53,10 +54,10 @@ class ContactData extends Component {
     }
     render(){
         let form = (<form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-            <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-            <input className={classes.Input} type="text" name="street" placeholder="Street" />
-            <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+            <Input inputtype='input' type="text" name="name" placeholder="Your Name" label='Name'/>
+            <Input inputtype='input' type="email" name="email" placeholder="Your Email" label='Email'/>
+            <Input inputtype='input' type="text" name="street" placeholder="Street" label='Street'/>
+            <Input inputtype='input' type="text" name="postal" placeholder="Postal Code" label='Postal Code'/>
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if(this.state.loading){
