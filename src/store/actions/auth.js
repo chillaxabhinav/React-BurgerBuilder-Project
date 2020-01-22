f3af2eaf44819064c3fd3a00093fb5f290814acb
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-01-22 12:17:13 +0530

    storing token and userid

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (idToken, userId) => {
     return {
         type : actionTypes.AUTH_SUCCESS,
-        authData : authData
+        idToken : idToken,
+        userId : userId
     };
 };
 
@@ -36,7 +37,7 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url,authData)
         .then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data))
+            dispatch(authSuccess(response.data.idToken, response.data.localId))
         })
         .catch(error => {
             console.log(error);
