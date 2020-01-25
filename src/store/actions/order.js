64e8bb89dcb938348efa1724e68187f6924a2793
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-01-26 02:15:34 +0530

    some more

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -64,10 +64,11 @@ export const fetchOrderStart = () =>{
     }
 }
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrderStart());
-        axios.get('/orders.json?auth='+ token)
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId+'"';
+        axios.get('/orders.json'+ queryParams)
             .then(response => {
                 const fetchedOrders = [];
                 console.log(response.data);
